e96b8e910b1ba7d5207efb090a0d01beba10f111
SANJAY OJHA <so6245877@gmail.com>
2025-08-14 21:00:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (32.72%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+//sanjay ojha 
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+        string prefix = strs[0] ;
+
+        for(int i=0; i<strs.size(); i++){
+            while(strs[i].find(prefix)!=0){
+                prefix = prefix.substr(0,prefix.size()-1);
+                if(prefix.empty()) return "";
+            }
+        }
+        return prefix;
+    }
+};
