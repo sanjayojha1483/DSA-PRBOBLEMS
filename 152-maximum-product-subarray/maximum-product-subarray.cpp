dd0705ed4b4a50a31e7001094ded9833fe4753d7
SANJAY OJHA <so6245877@gmail.com>
2025-10-30 23:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (24.04%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int curMax = nums[0];
+        int curMin = nums[0];
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.size(); ++i) {
+            int x = nums[i];
+            
+           
+            if (x < 0) swap(curMax, curMin);
+
+         
+            curMax = max(x, curMax * x);
+            curMin = min(x, curMin * x);
+
+            
+            ans = max(ans, curMax);
+        }
+
+        return ans;
+    }
+};
