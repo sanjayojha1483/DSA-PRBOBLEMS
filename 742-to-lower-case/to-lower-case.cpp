0e963b834da6479986d5c3b2daa01b285385c0e5
SANJAY OJHA <so6245877@gmail.com>
2025-11-06 21:59:25 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (97.87%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        transform(s.begin(),s.end(),s.begin(), ::tolower);
-        return s;
+      for(int i=0; i<s.size(); i++)
+      {
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] = s[i] + 32; 
+      }  
+      }
+      return s;
     }
 };
