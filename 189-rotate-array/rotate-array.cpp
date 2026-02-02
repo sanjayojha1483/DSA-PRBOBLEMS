e665298cfed46e6aef93f3a6c218e5ae03bfdd36
SANJAY OJHA <so6245877@gmail.com>
2026-02-02 18:17:46 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (68.04%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
+    void reverse(vector<int>& nums, int l,int r){
+        while(l<r){
+            swap(nums[l],nums[r]);
+                l++;
+                r--;
+            
+        }
+    }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;  // handle if k > n
-        vector<int> temp(n);
+        k = k%n;
+        //reverse whole array 1234567 -> 7654321
+        reverse(nums,0,n-1);
+        //reverse k elements  7654321->5674321
+        reverse(nums,0,k-1);
+        //reverse remaining 5674321 -> 5671234
+        reverse(nums,k,n-1);
         
-        for (int i = 0; i < n; i++) {
-            temp[(i + k) % n] = nums[i];
-        }
-        
-        nums = temp; // copy back
     }
 };
