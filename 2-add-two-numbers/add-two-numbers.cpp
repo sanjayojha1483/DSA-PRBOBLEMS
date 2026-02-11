ac4e09efb5bd6e078dd2621f8fdcf8f8703ff82d
SANJAY OJHA <so6245877@gmail.com>
2026-02-11 23:33:31 +0530

    Time: 0 ms (100.00%) | Memory: 77.2 MB (44.82%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,38 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
+        
+        ListNode* dummy = new ListNode(0); 
+        ListNode* temp = dummy;
         int carry = 0;
 
-        while(l1!=nullptr || l2!=nullptr || carry!=0){
-            int x = 0;
-            int y = 0;
-
-            if(l1!=nullptr){
-                x = l1->val;
+        while(l1 != NULL || l2 != NULL || carry) {
+            
+            int sum = carry;
+            
+            if(l1 != NULL) {
+                sum += l1->val;
                 l1 = l1->next;
             }
-            if(l2!=nullptr){
-                y = l2->val;
+            
+            if(l2 != NULL) {
+                sum += l2->val;
                 l2 = l2->next;
             }
-            int sum = x+y+carry;
-            carry = sum/10;
-            ListNode* newNode = new ListNode(sum%10);
-            curr->next = newNode;
-            curr = curr->next;
+
+            carry = sum / 10;
+            
+            temp->next = new ListNode(sum % 10);
+            temp = temp->next;
         }
+
         return dummy->next;
     }
 };
