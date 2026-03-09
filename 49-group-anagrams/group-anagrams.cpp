50799b568ab71d5975a2d123762c21505ea7505b
SANJAY OJHA <so6245877@gmail.com>
2026-03-09 23:23:39 +0530

    Time: 18 ms (49.64%) | Memory: 25 MB (66.37%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,25 +1,27 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
         unordered_map<string, vector<string>> mp;
-        
-        // Step 1: Traverse all strings
-        for (int i = 0; i < strs.size(); i++) {
+
+        for(int i = 0; i < strs.size(); i++) {
+
             string word = strs[i];
-            sort(word.begin(), word.end()); // sorted version
-            mp[word].push_back(strs[i]);    // group by sorted key
+            string key = word;
+
+            sort(key.begin(), key.end());
+
+            mp[key].push_back(word);
         }
 
-        // Step 2: collect all groups
-        vector<vector<string>> result;
+        vector<vector<string>> ans;
+
         unordered_map<string, vector<string>>::iterator it;
-        for (it = mp.begin(); it != mp.end(); it++) {
-            result.push_back(it->second);
+
+        for(it = mp.begin(); it != mp.end(); it++) {
+            ans.push_back(it->second);
         }
 
-        return result;
+        return ans;
     }
 };
